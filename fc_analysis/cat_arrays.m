a777be6d6039e7d63b14346351638556c925b5ff
Megan McMahon <meg.c.mcmahon@gmail.com>
2019-08-15 17:46:22 -0500

    removed old files

--- fc_analysis/cat_arrays.m
+++
@@ -1,4 +0,0 @@
-folder = '~/Box/ADM_Study/ADM/data/corr_numpyarrays';
-dircontent = dir(fullfile(folder, '*.csv')); 
-images = arrayfun(@(f) csvread(fullfile(folder, f.name)), dircontent, 'UniformOutput', false);
-image = cat(3, images{:});  
